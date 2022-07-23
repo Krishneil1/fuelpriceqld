7319a450ff5bf3cf6c5e43ed2523e177d2726ec1
krishneel-als <krishneel.kumar@alsglobal.com>
2022-07-24 09:36:19 +1000

    tables created succesffuly

---
+++ fuel.price.qld/Db/0004.sql
@@ -0,0 +1,23 @@
+﻿
+/****** Object:  Table [dbo].[GeographicRegions]    Script Date: 24/07/2022 9:30:30 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[GeographicRegions](
+	[GeographicRegionId] [int] NOT NULL,
+	[GeoRegionLevel] [int] NOT NULL,
+	[GeoRegionId] [int] NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Abbrev] [nvarchar](max) NULL,
+	[GeoRegionParentId] [nvarchar](max) NULL,
+ CONSTRAINT [PK_GeographicRegions] PRIMARY KEY CLUSTERED 
+(
+	[GeographicRegionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
---
+++ fuel.price.qld/Db/Program.cs
@@ -0,0 +1,38 @@
+﻿using DbUp.Engine;
+using DbUp;
+using System.Reflection;
+
+int result;
+
+string connectionString =
+    args.FirstOrDefault()
+    ?? "Server=LOCALHOST;Database=qld_fuel_price_v1;User ID=*****;Password=*****";
+
+UpgradeEngine upgrader = DeployChanges.To
+    .SqlDatabase(connectionString)
+    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+    .LogToConsole()
+    .Build();
+
+DatabaseUpgradeResult dbUpgradeResult = upgrader.PerformUpgrade();
+
+if (!dbUpgradeResult.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(dbUpgradeResult.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    result = -1;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Success!");
+    Console.ResetColor();
+
+    result = 0;
+}
+
+return result;
